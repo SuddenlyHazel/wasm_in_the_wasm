01a6b775555ae6583a8b92e7765579a4c2ed652d
Hazel <hazelrtech@gmail.com>
2021-03-20 00:45:39 -0400

    what am I doing

--- src/hello_rust/main.rs
+++ src/hello_rust/main.rs
@@ -10,20 +10,48 @@ fn init() {
 }
 
 #[query]
-fn add_one(v : u64) -> Nat {
+fn fib(v : u64) -> Nat {
     unsafe {
         let wats = r#"
-                (module
-                 (type $i64_=>_i64 (func (param i64) (result i64)))
-                 (memory $0 0)
-                 (export "addOne" (func $module/addOne))
-                 (export "memory" (memory $0))
-                 (func $module/addOne (param $0 i64) (result i64)
-                  local.get $0
-                  i64.const 1
-                  i64.add
-                 )
-                )
+            (module
+             (type $i64_=>_i64 (func (param i64) (result i64)))
+             (memory $0 0)
+             (export "fib" (func $module/fib))
+             (export "memory" (memory $0))
+             (func $module/fib (param $0 i64) (result i64)
+              (local $1 i32)
+              (local $2 i32)
+              (local $3 i32)
+              i32.const 1
+              local.set $1
+              local.get $0
+              i64.const 0
+              i64.ne
+              if
+               loop $while-continue|0
+                local.get $0
+                i64.const 1
+                i64.sub
+                local.tee $0
+                i64.const 0
+                i64.ne
+                if
+                 local.get $1
+                 local.get $2
+                 i32.add
+                 local.get $1
+                 local.set $2
+                 local.set $1
+                 br $while-continue|0
+                end
+               end
+               local.get $1
+               i64.extend_i32_s
+               return
+              end
+              i64.const 0
+             )
+            )
             "#;
 
         let binary = wat::parse_str(wats).unwrap();
@@ -43,7 +71,7 @@ fn add_one(v : u64) -> Nat {
                 .assert_no_start();
 
         let option = instance.invoke_export(
-            "addOne",
+            "fib",
             &[RuntimeValue::from(v)],
             &mut NopExternals,
         ).expect("failed to execute export").unwrap();
